2f061a47031b5d2c9b2092483c574a1fd515802b
Francesco <francesco@xevolab.com>
2024-10-23 11:54:47 -0700

    Add typedoc config file

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@
  * Author    : Francesco
  * Created at: 2024-03-23 20:56
  * Edited by : Francesco
- * Edited at : 2024-10-22 11:02
+ * Edited at : 2024-10-23 08:53
  *
  * Copyright (c) 2024 Xevolab S.R.L.
  */
 
 import OIDCClient from "./OIDCClient";
-
-export type * as types from "./types";
 
 export default OIDCClient;
 export { prodTrustAnchors, devTrustAnchors } from "./trustAnchors";
